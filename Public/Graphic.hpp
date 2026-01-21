c217dffa1bd1b19bca7fefb9bb7598b7e534ab59
Nanashi-Hax <nanashi.rainbowhacker@gmail.com>
2026-01-21 15:00:18 +0900

    Add namespace: Graphic

--- Public/Graphic.hpp
+++ Public/Graphic.hpp
@@ -4,70 +4,73 @@
 #include <string>
 #include <vector>
 
-class Shader
+namespace Graphic
 {
-public:
-    enum class AttributeFormat
-    {
-        SNorm8x1 = GX2_ATTRIB_FORMAT_SNORM_8,
-        SNorm8x2 = GX2_ATTRIB_FORMAT_SNORM_8_8,
-        SNorm8x4 = GX2_ATTRIB_FORMAT_SNORM_8_8_8_8,
-
-        UNorm8x1 = GX2_ATTRIB_FORMAT_UNORM_8,
-        UNorm8x2 = GX2_ATTRIB_FORMAT_UNORM_8_8,
-        UNorm8x4 = GX2_ATTRIB_FORMAT_UNORM_8_8_8_8,
-
-        SInt8x1 = GX2_ATTRIB_FORMAT_SINT_8,
-        SInt8x2 = GX2_ATTRIB_FORMAT_SINT_8_8,
-        SInt8x4 = GX2_ATTRIB_FORMAT_SINT_8_8_8_8,
-
-        UInt8x1 = GX2_ATTRIB_FORMAT_UINT_8,
-        UInt8x2 = GX2_ATTRIB_FORMAT_UINT_8_8,
-        UInt8x4 = GX2_ATTRIB_FORMAT_UINT_8_8_8_8,
-
-        Float32x1 = GX2_ATTRIB_FORMAT_FLOAT_32,
-        Float32x2 = GX2_ATTRIB_FORMAT_FLOAT_32_32,
-        Float32x3 = GX2_ATTRIB_FORMAT_FLOAT_32_32_32,
-        Float32x4 = GX2_ATTRIB_FORMAT_FLOAT_32_32_32_32
-    };
-
-    Shader(void const * file);
-    ~Shader();
-
-    // Initialize
-    void addAttribute(std::string const & name, uint32_t offset, AttributeFormat format);
-
-    // After setting attribute
-    void initFetch();
-
-    // Use
-    void use();
-
-private:
-    class Attribute
+    class Shader
     {
     public:
-        void add(GX2AttribStream const & stream, std::string const & name, uint32_t offset, AttributeFormat format);
-        std::vector<GX2AttribStream>& stream();
-        std::vector<std::string>& name();
-        std::vector<uint32_t>& offset();
-        std::vector<AttributeFormat>& format();
-
+        enum class AttributeFormat
+        {
+            SNorm8x1 = GX2_ATTRIB_FORMAT_SNORM_8,
+            SNorm8x2 = GX2_ATTRIB_FORMAT_SNORM_8_8,
+            SNorm8x4 = GX2_ATTRIB_FORMAT_SNORM_8_8_8_8,
+        
+            UNorm8x1 = GX2_ATTRIB_FORMAT_UNORM_8,
+            UNorm8x2 = GX2_ATTRIB_FORMAT_UNORM_8_8,
+            UNorm8x4 = GX2_ATTRIB_FORMAT_UNORM_8_8_8_8,
+        
+            SInt8x1 = GX2_ATTRIB_FORMAT_SINT_8,
+            SInt8x2 = GX2_ATTRIB_FORMAT_SINT_8_8,
+            SInt8x4 = GX2_ATTRIB_FORMAT_SINT_8_8_8_8,
+        
+            UInt8x1 = GX2_ATTRIB_FORMAT_UINT_8,
+            UInt8x2 = GX2_ATTRIB_FORMAT_UINT_8_8,
+            UInt8x4 = GX2_ATTRIB_FORMAT_UINT_8_8_8_8,
+        
+            Float32x1 = GX2_ATTRIB_FORMAT_FLOAT_32,
+            Float32x2 = GX2_ATTRIB_FORMAT_FLOAT_32_32,
+            Float32x3 = GX2_ATTRIB_FORMAT_FLOAT_32_32_32,
+            Float32x4 = GX2_ATTRIB_FORMAT_FLOAT_32_32_32_32
+        };
+    
+        Shader(void const * file);
+        ~Shader();
+    
+        // Initialize
+        void addAttribute(std::string const & name, uint32_t offset, AttributeFormat format);
+    
+        // After setting attribute
+        void initFetch();
+    
+        // Use
+        void use();
+    
     private:
-        std::vector<GX2AttribStream> _stream;
-        std::vector<std::string> _name;
-        std::vector<uint32_t> _offset;
-        std::vector<AttributeFormat> _format;
+        class Attribute
+        {
+        public:
+            void add(GX2AttribStream const & stream, std::string const & name, uint32_t offset, AttributeFormat format);
+            std::vector<GX2AttribStream>& stream();
+            std::vector<std::string>& name();
+            std::vector<uint32_t>& offset();
+            std::vector<AttributeFormat>& format();
+        
+        private:
+            std::vector<GX2AttribStream> _stream;
+            std::vector<std::string> _name;
+            std::vector<uint32_t> _offset;
+            std::vector<AttributeFormat> _format;
+        };
+    
+        void initVertex(void const * file);
+        void initPixel(void const * file);
+    
+        Attribute attribute;
+        GX2VertexShader * vertexShader;
+        GX2PixelShader * pixelShader;
+        GX2FetchShader * fetchShader;
+    
+        int getAttributeLocation(std::string const & name);
+        uint32_t getAttributeMask(AttributeFormat format);
     };
-
-    void initVertex(void const * file);
-    void initPixel(void const * file);
-
-    Attribute attribute;
-    GX2VertexShader * vertexShader;
-    GX2PixelShader * pixelShader;
-    GX2FetchShader * fetchShader;
-
-    int getAttributeLocation(std::string const & name);
-    uint32_t getAttributeMask(AttributeFormat format);
-};
+}
--- Source/Graphic.cpp
+++ Source/Graphic.cpp
@@ -11,208 +11,211 @@
 
 #include <whb/log.h>
 
-Shader::Shader(void const * file)
+namespace Graphic
 {
-    initPixel(file);
-    initVertex(file);
-}
-
-Shader::~Shader()
-{
-    if (vertexShader) MEMFreeToMappedMemory(vertexShader);
-    if (pixelShader) MEMFreeToMappedMemory(pixelShader);
-    if (fetchShader) MEMFreeToMappedMemory(fetchShader);
-}
-
-void Shader::addAttribute(std::string const & name, uint32_t offset, AttributeFormat format)
-{
-    GX2AttribStream stream;
-    stream.location = getAttributeLocation(name);
-    stream.buffer = 0;
-    stream.offset = offset;
-    stream.format = static_cast<GX2AttribFormat>(format);
-    stream.type = GX2_ATTRIB_INDEX_PER_VERTEX;
-    stream.aluDivisor = 0;
-    stream.mask = getAttributeMask(format);
-    stream.endianSwap = GX2_ENDIAN_SWAP_DEFAULT;
-
-    attribute.add(stream, name, offset, format);
-}
-
-void Shader::initFetch()
-{
-    fetchShader = reinterpret_cast<GX2FetchShader*>(MEMAllocFromMappedMemoryEx(sizeof(GX2FetchShader), 64));
-
-    uint32_t size = GX2CalcFetchShaderSizeEx(attribute.stream().size(), GX2_FETCH_SHADER_TESSELLATION_NONE, GX2_TESSELLATION_MODE_DISCRETE);
-    void * program = MEMAllocFromMappedMemoryForGX2Ex(size, GX2_SHADER_PROGRAM_ALIGNMENT);
-
-    GX2InitFetchShaderEx(fetchShader, reinterpret_cast<uint8_t*>(program), attribute.stream().size(), attribute.stream().data(), GX2_FETCH_SHADER_TESSELLATION_NONE, GX2_TESSELLATION_MODE_DISCRETE);
-
-    GX2Invalidate(GX2_INVALIDATE_MODE_CPU_SHADER, program, size);
-}
-
-void Shader::use()
-{
-    GX2SetVertexShader(vertexShader);
-    GX2SetPixelShader(pixelShader);
-    GX2SetFetchShader(fetchShader);
-}
-
-void Shader::initPixel(void const * file)
-{
-    uint32_t headerSize = GFDGetPixelShaderHeaderSize(0, file);
-    if (!headerSize)
+    Shader::Shader(void const * file)
     {
-        throw std::invalid_argument(std::format("{}: headerSize == 0", __FUNCTION__));
+        initPixel(file);
+        initVertex(file);
     }
-
-    uint32_t programSize = GFDGetPixelShaderProgramSize(0, file);
-    if (!programSize)
+    
+    Shader::~Shader()
     {
-        throw std::invalid_argument(std::format("{}: programSize == 0", __FUNCTION__));
+        if (vertexShader) MEMFreeToMappedMemory(vertexShader);
+        if (pixelShader) MEMFreeToMappedMemory(pixelShader);
+        if (fetchShader) MEMFreeToMappedMemory(fetchShader);
     }
-
-    GX2PixelShader * shader = reinterpret_cast<GX2PixelShader*>(MEMAllocFromMappedMemoryEx(headerSize, 64));
-    if (!shader)
+    
+    void Shader::addAttribute(std::string const & name, uint32_t offset, AttributeFormat format)
     {
-        throw std::invalid_argument(std::format("{}: MEMAllocFromMappedMemoryEx({}, 64) failed", __FUNCTION__, headerSize));
+        GX2AttribStream stream;
+        stream.location = getAttributeLocation(name);
+        stream.buffer = 0;
+        stream.offset = offset;
+        stream.format = static_cast<GX2AttribFormat>(format);
+        stream.type = GX2_ATTRIB_INDEX_PER_VERTEX;
+        stream.aluDivisor = 0;
+        stream.mask = getAttributeMask(format);
+        stream.endianSwap = GX2_ENDIAN_SWAP_DEFAULT;
+    
+        attribute.add(stream, name, offset, format);
     }
-
-    void * program = MEMAllocFromMappedMemoryForGX2Ex(programSize, GX2_SHADER_PROGRAM_ALIGNMENT);
-    if (!program)
+    
+    void Shader::initFetch()
     {
-        if(shader) MEMFreeToMappedMemory(shader);
-        throw std::invalid_argument(std::format("{}: MEMAllocFromMappedMemoryForGX2Ex failed", __FUNCTION__));
+        fetchShader = reinterpret_cast<GX2FetchShader*>(MEMAllocFromMappedMemoryEx(sizeof(GX2FetchShader), 64));
+    
+        uint32_t size = GX2CalcFetchShaderSizeEx(attribute.stream().size(), GX2_FETCH_SHADER_TESSELLATION_NONE, GX2_TESSELLATION_MODE_DISCRETE);
+        void * program = MEMAllocFromMappedMemoryForGX2Ex(size, GX2_SHADER_PROGRAM_ALIGNMENT);
+    
+        GX2InitFetchShaderEx(fetchShader, reinterpret_cast<uint8_t*>(program), attribute.stream().size(), attribute.stream().data(), GX2_FETCH_SHADER_TESSELLATION_NONE, GX2_TESSELLATION_MODE_DISCRETE);
+    
+        GX2Invalidate(GX2_INVALIDATE_MODE_CPU_SHADER, program, size);
     }
-
-    if (!GFDGetPixelShader(shader, program, 0, file))
+    
+    void Shader::use()
     {
-        if(shader) MEMFreeToMappedMemory(shader);
-        if(program) MEMFreeToMappedMemory(program);
-        GX2RUnlockBufferEx(&shader->gx2rBuffer, GX2R_RESOURCE_DISABLE_CPU_INVALIDATE | GX2R_RESOURCE_DISABLE_GPU_INVALIDATE);
-        throw std::invalid_argument(std::format("{}: GFDGetPixelShader failed", __FUNCTION__));
+        GX2SetVertexShader(vertexShader);
+        GX2SetPixelShader(pixelShader);
+        GX2SetFetchShader(fetchShader);
     }
-    pixelShader = shader;
-    pixelShader->size = programSize;
-
-    GX2Invalidate(GX2_INVALIDATE_MODE_CPU_SHADER, pixelShader->program, pixelShader->size);
-}
-
-void Shader::initVertex(void const * file)
-{
-    uint32_t headerSize = GFDGetVertexShaderHeaderSize(0, file);
-    if (!headerSize)
+    
+    void Shader::initPixel(void const * file)
     {
-        throw std::invalid_argument(std::format("{}: headerSize == 0", __FUNCTION__));
-    }
-
-    uint32_t programSize = GFDGetVertexShaderProgramSize(0, file);
-    if (!programSize)
-    {
-        throw std::invalid_argument(std::format("{}: programSize == 0", __FUNCTION__));
-    }
-
-    GX2VertexShader * shader = reinterpret_cast<GX2VertexShader*>(MEMAllocFromMappedMemoryEx(headerSize, 64));
-    if (!shader)
-    {
-        throw std::invalid_argument(std::format("{}: MEMAllocFromMappedMemoryEx({}, 64) failed", __FUNCTION__, headerSize));
-    }
-
-    void * program = MEMAllocFromMappedMemoryForGX2Ex(programSize, GX2_SHADER_PROGRAM_ALIGNMENT);
-    if (!program)
-    {
-        if(shader) MEMFreeToMappedMemory(shader);
-        throw std::invalid_argument(std::format("{}: MEMAllocFromMappedMemoryForGX2Ex failed", __FUNCTION__));
-    }
-
-    if (!GFDGetVertexShader(shader, program, 0, file))
-    {
-        if(shader) MEMFreeToMappedMemory(shader);
-        if(program) MEMFreeToMappedMemory(program);
-        GX2RUnlockBufferEx(&shader->gx2rBuffer, GX2R_RESOURCE_DISABLE_CPU_INVALIDATE | GX2R_RESOURCE_DISABLE_GPU_INVALIDATE);
-        throw std::invalid_argument(std::format("{}: GFDGetvertexShader failed", __FUNCTION__));
-    }
-    vertexShader = shader;
-    vertexShader->size = programSize;
-
-    GX2Invalidate(GX2_INVALIDATE_MODE_CPU_SHADER, vertexShader->program, vertexShader->size);
-}
-
-int Shader::getAttributeLocation(const std::string& name)
-{
-    for (uint32_t i = 0; i < vertexShader->attribVarCount; ++i)
-    {
-        if (vertexShader->attribVars[i].name == name)
+        uint32_t headerSize = GFDGetPixelShaderHeaderSize(0, file);
+        if (!headerSize)
         {
-            return vertexShader->attribVars[i].location;
+            throw std::invalid_argument(std::format("{}: headerSize == 0", __FUNCTION__));
+        }
+    
+        uint32_t programSize = GFDGetPixelShaderProgramSize(0, file);
+        if (!programSize)
+        {
+            throw std::invalid_argument(std::format("{}: programSize == 0", __FUNCTION__));
+        }
+    
+        GX2PixelShader * shader = reinterpret_cast<GX2PixelShader*>(MEMAllocFromMappedMemoryEx(headerSize, 64));
+        if (!shader)
+        {
+            throw std::invalid_argument(std::format("{}: MEMAllocFromMappedMemoryEx({}, 64) failed", __FUNCTION__, headerSize));
+        }
+    
+        void * program = MEMAllocFromMappedMemoryForGX2Ex(programSize, GX2_SHADER_PROGRAM_ALIGNMENT);
+        if (!program)
+        {
+            if(shader) MEMFreeToMappedMemory(shader);
+            throw std::invalid_argument(std::format("{}: MEMAllocFromMappedMemoryForGX2Ex failed", __FUNCTION__));
+        }
+    
+        if (!GFDGetPixelShader(shader, program, 0, file))
+        {
+            if(shader) MEMFreeToMappedMemory(shader);
+            if(program) MEMFreeToMappedMemory(program);
+            GX2RUnlockBufferEx(&shader->gx2rBuffer, GX2R_RESOURCE_DISABLE_CPU_INVALIDATE | GX2R_RESOURCE_DISABLE_GPU_INVALIDATE);
+            throw std::invalid_argument(std::format("{}: GFDGetPixelShader failed", __FUNCTION__));
+        }
+        pixelShader = shader;
+        pixelShader->size = programSize;
+    
+        GX2Invalidate(GX2_INVALIDATE_MODE_CPU_SHADER, pixelShader->program, pixelShader->size);
+    }
+    
+    void Shader::initVertex(void const * file)
+    {
+        uint32_t headerSize = GFDGetVertexShaderHeaderSize(0, file);
+        if (!headerSize)
+        {
+            throw std::invalid_argument(std::format("{}: headerSize == 0", __FUNCTION__));
+        }
+    
+        uint32_t programSize = GFDGetVertexShaderProgramSize(0, file);
+        if (!programSize)
+        {
+            throw std::invalid_argument(std::format("{}: programSize == 0", __FUNCTION__));
+        }
+    
+        GX2VertexShader * shader = reinterpret_cast<GX2VertexShader*>(MEMAllocFromMappedMemoryEx(headerSize, 64));
+        if (!shader)
+        {
+            throw std::invalid_argument(std::format("{}: MEMAllocFromMappedMemoryEx({}, 64) failed", __FUNCTION__, headerSize));
+        }
+    
+        void * program = MEMAllocFromMappedMemoryForGX2Ex(programSize, GX2_SHADER_PROGRAM_ALIGNMENT);
+        if (!program)
+        {
+            if(shader) MEMFreeToMappedMemory(shader);
+            throw std::invalid_argument(std::format("{}: MEMAllocFromMappedMemoryForGX2Ex failed", __FUNCTION__));
+        }
+    
+        if (!GFDGetVertexShader(shader, program, 0, file))
+        {
+            if(shader) MEMFreeToMappedMemory(shader);
+            if(program) MEMFreeToMappedMemory(program);
+            GX2RUnlockBufferEx(&shader->gx2rBuffer, GX2R_RESOURCE_DISABLE_CPU_INVALIDATE | GX2R_RESOURCE_DISABLE_GPU_INVALIDATE);
+            throw std::invalid_argument(std::format("{}: GFDGetvertexShader failed", __FUNCTION__));
+        }
+        vertexShader = shader;
+        vertexShader->size = programSize;
+    
+        GX2Invalidate(GX2_INVALIDATE_MODE_CPU_SHADER, vertexShader->program, vertexShader->size);
+    }
+    
+    int Shader::getAttributeLocation(const std::string& name)
+    {
+        for (uint32_t i = 0; i < vertexShader->attribVarCount; ++i)
+        {
+            if (vertexShader->attribVars[i].name == name)
+            {
+                return vertexShader->attribVars[i].location;
+            }
+        }
+        throw std::invalid_argument(std::format("Attribute name is invalid: {}", name));
+    }
+    
+    uint32_t Shader::getAttributeMask(AttributeFormat format)
+    {
+        switch (format)
+        {
+            case AttributeFormat::SNorm8x1:
+            case AttributeFormat::UNorm8x1:
+            case AttributeFormat::SInt8x1:
+            case AttributeFormat::UInt8x1:
+            case AttributeFormat::Float32x1:
+            {
+                return GX2_SEL_MASK(GX2_SQ_SEL_X, GX2_SQ_SEL_0, GX2_SQ_SEL_0, GX2_SQ_SEL_1);
+            }
+            case AttributeFormat::SNorm8x2:
+            case AttributeFormat::UNorm8x2:
+            case AttributeFormat::SInt8x2:
+            case AttributeFormat::UInt8x2:
+            case AttributeFormat::Float32x2:
+            {
+                return GX2_SEL_MASK(GX2_SQ_SEL_X, GX2_SQ_SEL_Y, GX2_SQ_SEL_0, GX2_SQ_SEL_1);
+            }
+            case AttributeFormat::Float32x3:
+            {
+                return GX2_SEL_MASK(GX2_SQ_SEL_X, GX2_SQ_SEL_Y, GX2_SQ_SEL_Z, GX2_SQ_SEL_1);
+            }
+            case AttributeFormat::SNorm8x4:
+            case AttributeFormat::UNorm8x4:
+            case AttributeFormat::SInt8x4:
+            case AttributeFormat::UInt8x4:
+            case AttributeFormat::Float32x4:
+            {
+                return GX2_SEL_MASK(GX2_SQ_SEL_X, GX2_SQ_SEL_Y, GX2_SQ_SEL_Z, GX2_SQ_SEL_W);
+            }
+            default:
+            {
+                return GX2_SEL_MASK(GX2_SQ_SEL_0, GX2_SQ_SEL_0, GX2_SQ_SEL_0, GX2_SQ_SEL_1);
+            }
         }
     }
-    throw std::invalid_argument(std::format("Attribute name is invalid: {}", name));
-}
-
-uint32_t Shader::getAttributeMask(AttributeFormat format)
-{
-    switch (format)
+    
+    void Shader::Attribute::add(GX2AttribStream const & stream, std::string const & name, uint32_t offset, AttributeFormat format)
     {
-        case AttributeFormat::SNorm8x1:
-        case AttributeFormat::UNorm8x1:
-        case AttributeFormat::SInt8x1:
-        case AttributeFormat::UInt8x1:
-        case AttributeFormat::Float32x1:
-        {
-            return GX2_SEL_MASK(GX2_SQ_SEL_X, GX2_SQ_SEL_0, GX2_SQ_SEL_0, GX2_SQ_SEL_1);
-        }
-        case AttributeFormat::SNorm8x2:
-        case AttributeFormat::UNorm8x2:
-        case AttributeFormat::SInt8x2:
-        case AttributeFormat::UInt8x2:
-        case AttributeFormat::Float32x2:
-        {
-            return GX2_SEL_MASK(GX2_SQ_SEL_X, GX2_SQ_SEL_Y, GX2_SQ_SEL_0, GX2_SQ_SEL_1);
-        }
-        case AttributeFormat::Float32x3:
-        {
-            return GX2_SEL_MASK(GX2_SQ_SEL_X, GX2_SQ_SEL_Y, GX2_SQ_SEL_Z, GX2_SQ_SEL_1);
-        }
-        case AttributeFormat::SNorm8x4:
-        case AttributeFormat::UNorm8x4:
-        case AttributeFormat::SInt8x4:
-        case AttributeFormat::UInt8x4:
-        case AttributeFormat::Float32x4:
-        {
-            return GX2_SEL_MASK(GX2_SQ_SEL_X, GX2_SQ_SEL_Y, GX2_SQ_SEL_Z, GX2_SQ_SEL_W);
-        }
-        default:
-        {
-            return GX2_SEL_MASK(GX2_SQ_SEL_0, GX2_SQ_SEL_0, GX2_SQ_SEL_0, GX2_SQ_SEL_1);
-        }
+        _stream.push_back(stream);
+        _name.push_back(name);
+        _offset.push_back(offset);
+        _format.push_back(format);
     }
-}
-
-void Shader::Attribute::add(GX2AttribStream const & stream, std::string const & name, uint32_t offset, AttributeFormat format)
-{
-    _stream.push_back(stream);
-    _name.push_back(name);
-    _offset.push_back(offset);
-    _format.push_back(format);
-}
-
-std::vector<GX2AttribStream>& Shader::Attribute::stream()
-{
-    return _stream;
-}
-
-std::vector<std::string>& Shader::Attribute::name()
-{
-    return _name;
-}
-
-std::vector<uint32_t>& Shader::Attribute::offset()
-{
-    return _offset;
-}
-
-std::vector<Shader::AttributeFormat>& Shader::Attribute::format()
-{
-    return _format;
+    
+    std::vector<GX2AttribStream>& Shader::Attribute::stream()
+    {
+        return _stream;
+    }
+    
+    std::vector<std::string>& Shader::Attribute::name()
+    {
+        return _name;
+    }
+    
+    std::vector<uint32_t>& Shader::Attribute::offset()
+    {
+        return _offset;
+    }
+    
+    std::vector<Shader::AttributeFormat>& Shader::Attribute::format()
+    {
+        return _format;
+    }
 }
